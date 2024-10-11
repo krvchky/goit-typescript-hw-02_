e268f9050c3b5a3ff244a76545e439ef87898125
krvchky <kravchukyana05@gmail.com>
2024-10-11 13:56:42 +0200

    Update SearchBar.tsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,3 +1,60 @@
+// import React from "react";
+// import { Formik, Form, Field } from "formik";
+// import * as Yup from "yup"; 
+// import { IoSearchOutline } from "react-icons/io5";
+// import s from "./SearchBar.module.css";
+
+// const validationSchema = Yup.object({
+//   query: Yup.string()
+//     .required("Search query is required") 
+//     .min(3, "Search query must be at least 3 characters"),
+// });
+
+// interface SearchBarProps {
+//   setQuery: (query: string) => void;
+// }
+// const SearchBar: React.FC<SearchBarProps> = ({ setQuery }) => {
+//   const initialValues = {
+//     query: "",
+//   };
+
+//   const handleSubmit = (values: { query: string }) => {
+//     console.log(values);
+//     setQuery(values.query);
+//   };
+
+//   return (
+//     <header className={s.header}>
+//       <Formik
+//         initialValues={initialValues}
+//         onSubmit={handleSubmit}
+//         validationSchema={validationSchema} 
+//       >
+//         {({ errors, touched }) => (
+//           <Form className={s.form}>
+//             <div className={s.inputWrapper}>
+//               <Field
+//                 name="query"
+//                 className={s.input}
+//                 type="search"
+//                 placeholder="Search images and photos"
+//               />
+//             //   {errors.query && touched.query ? (
+//             //     <div className={s.error}>{errors.query}</div>  validation error
+//             //   ) : null}
+//               <button className={s.btn}type="submit">
+//                 <IoSearchOutline />
+//               </button>
+//             </div>
+//           </Form>
+//         )}
+//       </Formik>
+//     </header>
+//   );
+// };
+
+// export default SearchBar;
+
 import React from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup"; 
@@ -39,10 +96,10 @@ const SearchBar: React.FC<SearchBarProps> = ({ setQuery }) => {
                 type="search"
                 placeholder="Search images and photos"
               />
-            //   {errors.query && touched.query ? (
-            //     <div className={s.error}>{errors.query}</div>  validation error
-            //   ) : null}
-              <button className={s.btn}type="submit">
+              {errors.query && touched.query ? (
+                <div className={s.error}>{errors.query}</div>
+              ) : null}
+              <button className={s.btn} type="submit">
                 <IoSearchOutline />
               </button>
             </div>
